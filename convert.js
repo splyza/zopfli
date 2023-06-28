4079f686e0fc079a543bd83c89ca9b01497cdf2e
Guillermo Martel <guillermo.martel@splyza.com>
2023-06-28 14:49:00 +0900

    adding convertjs script

---
+++ convert.js
@@ -0,0 +1,41 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+try {
+	main();
+} catch (err) {
+	console.log(`error: `);
+	console.log(main);
+}
+
+
+function main() {
+	var files = [];
+
+	fs.readdirSync('./orig').forEach(file => {
+	  	if ( file.indexOf('.png') > -1 ) {
+	    	files.push(file);
+		}
+	});
+
+	console.log(files.length);
+
+	for ( var x = 0, length = files.length; x < length; x++) {
+		zopflipng(files[x]);
+	}
+
+
+	function zopflipng(fileName) {
+		const child = exec(
+		'./zopflipng orig/' + fileName + ' out/' + fileName + ' ',
+	    (error, stdout, stderr) => {
+	    	console.log(`stdout: ---- ` + fileName);
+	        console.log(`${stdout}`);
+	        
+	        if (error !== null) {
+	        	console.log(`stderr: ${stderr}`);
+	            console.log(`exec error: ${error}`);
+	        }
+		});
+	}
+}
